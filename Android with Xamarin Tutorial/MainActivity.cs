ee81483d29b4ca1012088369192776ce73c6fd8a
Tron <Tron@PARTICLE-DRIVE>
2017-01-31 14:07:21 -0600

    Add project files.

---
+++ Android with Xamarin Tutorial/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Android_with_Xamarin_Tutorial
+{
+    [Activity(Label = "Android_with_Xamarin_Tutorial", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
